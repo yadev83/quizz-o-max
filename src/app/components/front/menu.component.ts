910ae4a80d1c114c190d0f867148476eb7b5aec5
yadev83 <50366613+yadev83@users.noreply.github.com>
2020-12-20 19:04:43 +0100

    Highscore page now fake. Waiting for highscore functionnality to display scores

--- src/app/components/front/menu.component.ts
+++ src/app/components/front/menu.component.ts
@@ -7,7 +7,7 @@ import { MatDrawer } from '@angular/material/sidenav';
   selector: 'qom-menu',
   template: `
     <div id="menu-content">
-      <a routerLink="/home" routerLinkActive="selected-menu"><button mat-button class="qom-menu-btn">Index</button></a>
+      <a routerLink="/home" routerLinkActive="selected-menu"><button mat-button class="qom-menu-btn">Home</button></a>
       <a routerLink="/play" routerLinkActive="selected-menu"><button mat-button class="qom-menu-btn">Play</button></a>
       <a routerLink="/scores" routerLinkActive="selected-menu"><button mat-button class="qom-menu-btn">Highscores</button></a>
       <a routerLink="/about" routerLinkActive="selected-menu"><button mat-button class="qom-menu-btn">About</button></a>
--- src/app/components/front/scores.component.ts
+++ src/app/components/front/scores.component.ts
@@ -3,9 +3,113 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'qom-scores',
   template: `
-    <p>
-      scores works!
-    </p>
+    <div class="flex-container-col center">
+      <div class="flex-item">
+        <h1>Highscores for Category 0</h1>
+        <table>
+          <tr>
+            <th>Username</th>
+            <th>Time spent (mm:ss)</th>
+            <th>Answer ratio</th>
+            <th>Final Score</th>
+          </tr>
+          <tr>
+            <td>John Doe</td>
+            <td>12:50</td>
+            <td>8/10</td>
+            <td>650</td>
+          </tr>
+          <tr>
+            <td>John Doe</td>
+            <td>12:50</td>
+            <td>8/10</td>
+            <td>650</td>
+          </tr>
+          <tr>
+            <td>John Doe</td>
+            <td>12:50</td>
+            <td>8/10</td>
+            <td>650</td>
+          </tr>
+          <tr>
+            <td>John Doe</td>
+            <td>12:50</td>
+            <td>8/10</td>
+            <td>650</td>
+          </tr>
+        </table>
+      </div>
+      <div class="flex-item">
+        <h1>Highscores for Category 1</h1>
+        <table>
+          <tr>
+            <th>Username</th>
+            <th>Time spent (mm:ss)</th>
+            <th>Answer ratio</th>
+            <th>Final Score</th>
+          </tr>
+          <tr>
+            <td>John Doe</td>
+            <td>12:50</td>
+            <td>8/10</td>
+            <td>650</td>
+          </tr>
+          <tr>
+            <td>John Doe</td>
+            <td>12:50</td>
+            <td>8/10</td>
+            <td>650</td>
+          </tr>
+          <tr>
+            <td>John Doe</td>
+            <td>12:50</td>
+            <td>8/10</td>
+            <td>650</td>
+          </tr>
+          <tr>
+            <td>John Doe</td>
+            <td>12:50</td>
+            <td>8/10</td>
+            <td>650</td>
+          </tr>
+        </table>
+      </div>
+      <div class="flex-item">
+        <h1>Highscores for Category 2</h1>
+        <table>
+          <tr>
+            <th>Username</th>
+            <th>Time spent (mm:ss)</th>
+            <th>Answer ratio</th>
+            <th>Final Score</th>
+          </tr>
+          <tr>
+            <td>John Doe</td>
+            <td>12:50</td>
+            <td>8/10</td>
+            <td>650</td>
+          </tr>
+          <tr>
+            <td>John Doe</td>
+            <td>12:50</td>
+            <td>8/10</td>
+            <td>650</td>
+          </tr>
+          <tr>
+            <td>John Doe</td>
+            <td>12:50</td>
+            <td>8/10</td>
+            <td>650</td>
+          </tr>
+          <tr>
+            <td>John Doe</td>
+            <td>12:50</td>
+            <td>8/10</td>
+            <td>650</td>
+          </tr>
+        </table>
+      </div>
+    </div>
   `,
   styles: [
   ]
