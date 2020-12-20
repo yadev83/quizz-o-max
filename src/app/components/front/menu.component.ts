ed4602a2de8255c54217c0007f090aaec92427c0
yadev83 <50366613+yadev83@users.noreply.github.com>
2020-12-20 15:39:40 +0100

    Styles now working again (was an error on the links added in the menu...)

--- src/app/components/front/menu.component.ts
+++ src/app/components/front/menu.component.ts
@@ -7,15 +7,15 @@ import { MatDrawer } from '@angular/material/sidenav';
   selector: 'qom-menu',
   template: `
     <div id="menu-content">
-      <a routerLink="/home" routerLinkActive="selected-menu"><button mat-button>Index</button></a>
-      <a routerLink="/play" routerLinkActive="selected-menu"><button mat-button>Play</button></a>
-      <a routerLink="/scores" routerLinkActive="selected-menu"><button mat-button>Highscores</button></a>
-      <a routerLink="/about" routerLinkActive="selected-menu"><button mat-button>About</button></a>
+      <a routerLink="/home" routerLinkActive="selected-menu"><button mat-button class="qom-menu-btn">Index</button></a>
+      <a routerLink="/play" routerLinkActive="selected-menu"><button mat-button class="qom-menu-btn">Play</button></a>
+      <a routerLink="/scores" routerLinkActive="selected-menu"><button mat-button class="qom-menu-btn">Highscores</button></a>
+      <a routerLink="/about" routerLinkActive="selected-menu"><button mat-button class="qom-menu-btn">About</button></a>
     </div>
   `,
   styles: [
     "#menu-content{width:250px;}",
-    "#menu-content>*{width:100%;}"
+    ".qom-menu-btn{width:100%;color:black;}"
   ]
 })
 export class MenuComponent implements OnInit {
