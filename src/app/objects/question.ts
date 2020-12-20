11dcc0719e70a9145dbace54b007350b76ebead9
yadev83 <50366613+yadev83@users.noreply.github.com>
2020-12-20 19:38:02 +0100

    Did some formattin on strings containing special chars.

--- src/app/objects/question.ts
+++ src/app/objects/question.ts
@@ -12,14 +12,23 @@ export class Question{
     this.category = category;
     this.type = type;
     this.difficulty = difficulty;
-    this.question = question;
-    this.correct_answer = correct_answer;
+    this.question = this.format_html(question);
+    this.correct_answer =  this.format_html(correct_answer);
     this.incorrect_answers = incorrect_answers;
 
     this.answers = [];
     for(let option of incorrect_answers){
-      this.answers.push(option);
+      this.answers.push(this.format_html(option));
     }
-    this.answers.push(correct_answer);
+    this.answers.push(this.correct_answer);
+  }
+
+  private format_html(str): string{
+    let result : string = str.replace(/&quot;/g, '\"');
+    result = result.replace(/&ldquo;/g, '\“');
+    result = result.replace(/&rdquo;/g, '\”');
+    result = result.replace(/&#039;/g, '\'');
+
+    return result;
   }
 };
